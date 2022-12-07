d37da793f333855c5ad8269144f66c6ac7157969
Askhat Aubakirov <105732426+askhat-aubakirov@users.noreply.github.com>
2022-12-07 12:49:55 +0600

    Add files via upload

---
+++ exercises/aidar_aubakirov/problem1/bioinftry3.py
@@ -0,0 +1,20 @@
+
+import random
+
+len_mol1 = 121024
+len_mol = len_mol1
+time = 10800
+mass_one = 345
+count = 0
+for i in range(time):
+    speed = random.randint(50, 100)
+    len_mol = len_mol - speed
+    count += 1
+    if len_mol <= 0:
+        break
+    else:
+        continue
+nukl_num = 2**(time/count)
+total_mass = len_mol1 * 2 * mass_one * nukl_num
+print(len(str(total_mass)))
+
